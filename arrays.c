37e38eac5c442391c17390e8c5845c5edb95fde0
Akrampatel <43269095+Akrampatel@users.noreply.github.com>
2018-09-30 16:01:46 +0530

    Create arrays.c

---
+++ arrays.c
@@ -0,0 +1,14 @@
+/* finding average using arrays this is 2nd program I am writing using C language */
+
+#include <stdio.h>
+int main() {
+int average;
+int grades[3];
+grades[0] = 90;
+grades[1] = 85;
+grades[2] = 80;
+
+average = (grades[0] + grades[1] + grades[2]) / 3;
+printf("The average for 3 grades is: %d \n", average)
+return 0;
+}
