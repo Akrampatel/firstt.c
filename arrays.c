6b8a3a12d5550d4881defb133e5c0f1980765a08
Akrampatel <43269095+Akrampatel@users.noreply.github.com>
2018-09-30 16:04:07 +0530

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -9,6 +9,6 @@ grades[1] = 85;
 grades[2] = 80;
 
 average = (grades[0] + grades[1] + grades[2]) / 3;
-printf("The average for 3 grades is: %d \n", average)
+printf("The average for 3 grades is: %d \n", average);
 return 0;
 }
