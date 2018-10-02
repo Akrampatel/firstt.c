c061142ce204bdf8db0c7640be0bbd78ca0547ef
Akrampatel <43269095+Akrampatel@users.noreply.github.com>
2018-10-02 15:05:32 +0530

    Create simple calculator.c

---
+++ simple calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+double sumPlus;
+double sumPlus1;
+
+    printf("Enter first number that you want to sum: ");
+    scanf("%lf", &sumPlus);
+    printf("Enter second number that you want to sum: ");
+scanf("%lf", &sumPlus1);
+printf("Total is: %f", sumPlus + sumPlus1);
+    return 0;
+}
