270b6688da9dc5f85877915fd16130301a9e2bf8
Akrampatel <43269095+Akrampatel@users.noreply.github.com>
2018-10-03 16:17:20 +0530

    Create arraysBasic.c

---
+++ arraysBasic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int randomNumber[] = {3,5,6,7,2,9,1,0};
+    printf("Please enter any key to get your today's lucky number: ");
+    scanf("Your today's lucky number is : '%d'.");
+    printf("Your today's lucky number is : '%d'", randomNumber[0]);
+
+
+    return 0;
+}
+
+
+
+
